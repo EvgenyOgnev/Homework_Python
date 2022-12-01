e379e7000ce4a8b96b808bda3362b093d391bfeb
Evgeny Ognev <evgenyiognev-nn@mail.ru>
2022-12-01 19:20:25 +0300

    задача 2_5

--- Task_2_5.py
+++ Task_2_5.py
@@ -1,7 +1,12 @@
 # 5. Реализуйте алгоритм перемешивания списка.
 
+n = int(input('Введите число: '))
 
+b = []
+for _ in range(-n, n + 1, 2):
+    b.append(_)
+print(b)
 
-
-
-
+import random
+random.shuffle(b)
+print(b)
